53cbe6cda2db775abb2a05036767bada0a5219cf
BeigeBearDev <carneo.dev@gmail.com>
2024-12-10 10:16:58 +0900

    Initial commit

---
+++ src/main/java/com/bbd/board/SpringbootBoard20241210Application.java
@@ -0,0 +1,13 @@
+package com.bbd.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBoard20241210Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBoard20241210Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/bbd/board/SpringbootBoard20241210ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbd.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBoard20241210ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
