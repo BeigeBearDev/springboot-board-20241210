99d21de718bbe354681f644a1880b9e26843aed4
BeigeBearDev <carneo.dev@gmail.com>
2024-12-12 09:49:19 +0900

    feat: 2-05 Repository Database - Add QuestionRepository Interface - Add AnswerRepository Interface - fix .gitignore - rename test.java - add Junit

--- src/test/java/com/bbd/board/SbbApplicationTests.java
+++ src/test/java/com/bbd/board/SbbApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbootBoard20241210ApplicationTests {
+class SbbApplicationTests {
 
     @Test
     void contextLoads() {
